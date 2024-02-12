274a5da91cf1dd2445736501a1ab3f0eb1f5790d
Julian Ortega <julianortega746@gmail.com>
2024-02-12 13:20:28 -0300

    Probando el commit

---
+++ src/main/java/proyecton/com/Proyecton7/Proyecton777Application.java
@@ -0,0 +1,17 @@
+package proyecton.com.Proyecton7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecton777Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecton777Application.class, args);
+                System.out.println("hello wolrd");
+                if (true) {
+                    System.out.println("hello");
+                }
+	}
+
+}
