8904814ac9e50cfdeae5bcd638b99d70e2d64220
Julian Ortega <julianortega746@gmail.com>
2024-02-12 13:51:30 -0300

    Lista para trabajar

--- src/main/java/proyecton/com/Proyecton7/Proyecton777Application.java
+++ src/main/java/proyecton/com/Proyecton7/Proyecton777Application.java
@@ -8,10 +8,7 @@ public class Proyecton777Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Proyecton777Application.class, args);
-                System.out.println("hello wolrd");
-                if (true) {
-                    System.out.println("hello");
-                }
-	}
+        
+        }
 
 }
