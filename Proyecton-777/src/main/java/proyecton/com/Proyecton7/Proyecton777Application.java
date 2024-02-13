a729e5d48c3bb2eca5ff10e04ab84ad1b84b7e92
g__john <juansgb316@outlook.com>
2024-02-13 16:13:55 -0300

    no merge

--- Proyecton-777/src/main/java/proyecton/com/Proyecton7/Proyecton777Application.java
+++ Proyecton-777/src/main/java/proyecton/com/Proyecton7/Proyecton777Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Proyecton777Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Proyecton777Application.class, args);
 	}
-
 }
