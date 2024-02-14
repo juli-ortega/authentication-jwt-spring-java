1a62408712dcabec21f260f0badfe1fa9962d0aa
g__john <juansgb316@outlook.com>
2024-02-14 20:15:39 -0300

    register y signin html creado, haciendo userService

--- Proyecton-777/src/main/java/proyecton/com/Proyecton7/controladores/homeController.java
+++ Proyecton-777/src/main/java/proyecton/com/Proyecton7/controladores/homeController.java
@@ -2,6 +2,7 @@ package proyecton.com.Proyecton7.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,13 +12,4 @@ public class homeController {
     public String index() {
         return "index.html";
     }
-    @GetMapping("/iniciar")
-    public String iniciar() {
-        return "iniciar.html";
-    }
-    @GetMapping("/registrarse")
-    public String registrarse() {
-        return "registrase.html";
-    }
-    
 }
