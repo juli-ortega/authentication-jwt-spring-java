09f999218df0db57d9fc0976acf68101811bee8b
Herrera <urielherrera412@gmail.com>
2024-02-15 18:33:59 -0300

    formato de carpetas arregladas

---
+++ src/main/java/proyecton/com/Proyecton7/entities/Usuario.java
@@ -0,0 +1,31 @@
+package proyecton.com.Proyecton7.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import proyecton.com.Proyecton7.enumeraciones.Roles;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    private String dni;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private String phone_number;
+    @Enumerated(EnumType.STRING)
+    private Roles rol;
+    private Boolean alta;
+
+}
