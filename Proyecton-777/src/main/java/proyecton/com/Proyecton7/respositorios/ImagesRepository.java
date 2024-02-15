e964266fff60ce976316dd2068b53f19276cd0b6
Herrera <urielherrera412@gmail.com>
2024-02-15 16:51:46 -0300

    lol2

--- Proyecton-777/src/main/java/proyecton/com/Proyecton7/respositorios/ImagesRepository.java
+++
@@ -1,8 +0,0 @@
-package proyecton.com.Proyecton7.respositorios;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import proyecton.com.Proyecton7.entities.Images;
-
-@Repository
-public interface ImagesRepository extends JpaRepository<Images, String> {
-}
