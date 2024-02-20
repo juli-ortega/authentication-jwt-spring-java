d6e2419ac4f36733db7f59302c50a3af4ab9702c
Gabriel <gabriel.kitanovich@gmail.com>
2024-02-20 16:38:54 -0300

    actualización de la pagina principal + inicio de sesión

--- src/main/resources/static/css/js/01.js
+++ src/main/resources/static/css/js/01.js
@@ -6,3 +6,19 @@ window.addEventListener("scroll", function () {
 
 const info = document.querySelector(".informacion")
 
+function loadPopupContent(pageUrl) {
+    var popupContent = document.getElementById("popup-content");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                popupContent.innerHTML = xhr.responseText;
+                popupContent.style.display = "block"; // Mostrar el contenido del menú desplegable
+            } else {
+                console.error("Error al cargar la página externa");
+            }
+        }
+    };
+    xhr.open("GET", pageUrl, true);
+    xhr.send();
+}
