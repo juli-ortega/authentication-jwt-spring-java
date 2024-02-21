2887f693adf654f6bc357401d2a16aa039a5702e
Gabriel <gabriel.kitanovich@gmail.com>
2024-02-21 20:04:42 -0300

    actualización estetica, nuevo js y iniciar sesión popup

--- src/main/resources/static/css/js/01.js
+++ src/main/resources/static/css/js/01.js
@@ -1,24 +1,6 @@
-const nav = document.querySelector(".nav")
+
+const nav = document.querySelector(".nav");
 
 window.addEventListener("scroll", function () {
     nav.classList.toggle("active", window.scrollY > 0)
-})
-
-const info = document.querySelector(".informacion")
-
-function loadPopupContent(pageUrl) {
-    var popupContent = document.getElementById("popup-content");
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                popupContent.innerHTML = xhr.responseText;
-                popupContent.style.display = "block"; // Mostrar el contenido del menú desplegable
-            } else {
-                console.error("Error al cargar la página externa");
-            }
-        }
-    };
-    xhr.open("GET", pageUrl, true);
-    xhr.send();
-}
+});
